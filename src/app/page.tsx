f72575b59b899ee8be17691ada7f81fb343f1789
Z User <z@container>
2026-06-07 20:07:11 +0000

    06e17cd8-2466-44b3-9e2f-c0bf380de744

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import { Process } from "@/components/stackforge/process";
 import { Testimonials } from "@/components/stackforge/testimonials";
 import { TechStack } from "@/components/stackforge/tech-stack";
 import { FAQ } from "@/components/stackforge/faq";
-import { Pricing } from "@/components/stackforge/pricing";
 import { CtaBanner } from "@/components/stackforge/cta-banner";
 import { Contact } from "@/components/stackforge/contact";
 import { Footer } from "@/components/stackforge/footer";
@@ -40,8 +39,6 @@ export default function Home() {
         <SectionDivider />
         <TechStack />
         <FAQ />
-        <SectionDivider />
-        <Pricing />
         <CtaBanner />
         <SectionDivider />
         <Contact />
